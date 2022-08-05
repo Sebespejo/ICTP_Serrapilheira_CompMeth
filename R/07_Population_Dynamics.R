b89dabf195d32cb63f53c33cce386f6de3c23ed9
Sebespejo <sebespejo0@gmail.com>
2022-08-05 18:08:40 -0300

    Adjustments to models

--- R/07_Population_Dynamics.R
+++ R/07_Population_Dynamics.R
@@ -4,8 +4,8 @@ library(deSolve)
 library(ggplot2) # because we will plot things
 library(tidyr) # because we will manipulate some data
 
-#source("fct/logGrowth.R")
-#source("fct/LVComp.R")
+source("fct/logGrowth.R")
+source("fct/LVComp.R")
 
 
 #Logistic model
@@ -25,7 +25,7 @@ p <- c(r = 0.1, a = 0.05)
 y0 <- c(N = 10)
 ## time steps
 t <- 1:200
-  #you can alsoo create sequences like seq(1,200,by=.1)
+#you can also create sequences like seq(1,200,by=.1)
 
 ## give the function and the parameters to the ode function
 out_log <- ode(y = y0, times = t, func = logGrowth, parms = p)
@@ -68,4 +68,40 @@ ggplot(df_lv) +
   geom_line(aes(x = time, y = value, color = name)) +
   labs(x = "Time", y = "N", color = "Species") +
   theme_classic()
+
+#MacArthurModel
+RwMcPred <- function(t, y, p) {
+  N <- y[1] ; P <- y[2]
+  with(as.list(p), {
+    dN.dt <- (r1 * N * (1-N/K)) - ((c*N*P)/(1+c*tau*N))
+    dP.dt <- ((b * N * P) / (1+c*tau*N))- d * P
+    return(list(c(dN.dt, dP.dt)))
+  })
+}
+
+# RwMc parameters
+c <- 2 #PconsumptionRate
+b <- 1 #c convertion coef
+tau <- 0.2 #manipulationTime
+d <- 0.2 #P deathRate
+a1 <- list(c(c,b,tau,d))
+# here we have to add the predation coefficients, c, b, tau and d which are consumption, converted consumption and manipulationTime and PdeathRate respectively
+r1 <- 10 # growthRate N
+p3 <- list(r1, a1)
+N1 <- c(5000, 100)
+t2 <- c(1:1000)
+K <- 15000
+
+out_RwMc <- ode(y = N1, times = t2, func = RwMcPred, parms = p3)
+
+#head(out_RwMc)
+
+df_RwMc <- pivot_longer(as.data.frame(out_RwMc), cols = 2:3)
+
+#Plot
+ggplot(tb_RwMc) +
+  geom_line(aes(x = time, y = value, color = name)) +
+  labs(x = "Time", y = "N", color = "Species") +
+  theme_classic()
+
 
